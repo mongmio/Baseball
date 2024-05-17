8214181fde78219cadc180f2c56d1f8529a6ec69
Youngjoo <my.cat.liang@gmail.com>
2024-05-17 14:06:54 +0900

    [refactoring] - introduce test fixture

--- Test/BaseballTest.cpp
+++ Test/BaseballTest.cpp
@@ -1,14 +1,17 @@
 #include "pch.h"
 #include "../Baseball/Baseball.cpp"
 
-TEST(BaseballTest, InvaidLengthOfParameter) {
+class BaseballTest : public testing::Test
+{
+protected:
 	Baseball game{"123"};
+};
+TEST_F(BaseballTest, InvaidLengthOfParameter) {
 	EXPECT_THROW(game.guess("12"), length_error);
 	EXPECT_THROW(game.guess("1212"), length_error);
 }
 
-TEST(BaseballTest, InvaidArgumentOfParameter) {
-	Baseball game{ "123" };
+TEST_F(BaseballTest, InvaidArgumentOfParameter) {
 	EXPECT_THROW(game.guess("12s"), invalid_argument);
 	EXPECT_THROW(game.guess("aaa"), invalid_argument);
 }
