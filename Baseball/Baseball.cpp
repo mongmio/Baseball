75c5e62fceae7aeb3f1cfc5de37feaf4df7c5c8c
Youngjoo <my.cat.liang@gmail.com>
2024-05-17 14:22:59 +0900

    [refactoring] guess

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -8,21 +8,27 @@ class Baseball
 public:
 	Baseball(string _solution);
 	~Baseball();
-	bool guess(string str)
+	bool guess(string guessing)
 	{
-		if (3 != str.size())
+		verifyArgument(guessing);
+		return false;
+	}
+private:
+	void verifyArgument(std::string& guessing)
+	{
+		if (3 != guessing.size())
 		{
 			throw length_error("must be 3 letters");
 		}
-		for (auto ch : str)
+		for (auto ch : guessing)
 		{
 			if ('0' > ch || '9' < ch)
 			{
 				throw invalid_argument("must be number");
 			}
 		}
-		bool existence[10]{false};
-		for (auto ch : str)
+		bool existence[10]{ false };
+		for (auto ch : guessing)
 		{
 			if (existence[(ch - '0')] == true)
 			{
@@ -30,9 +36,7 @@ public:
 			}
 			existence[(ch - '0')] = true;
 		}
-		return false;
 	}
-private:
 	string solution;
 	
 };
