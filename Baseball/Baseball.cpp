296f1fcf553f1b4709b0a0a410bc34a09773c4f6
Youngjoo <my.cat.liang@gmail.com>
2024-05-17 14:53:42 +0900

    [refactoring] - Baseball class

--- Baseball/Baseball.cpp
+++ Baseball/Baseball.cpp
@@ -13,63 +13,25 @@ class Baseball
 {
 public:
 	Baseball(string _solution);
+	void buildSolutionMap(string& solution);
 	~Baseball();
-	GuessResult guess(string guessing)
-	{
-		verifyArgument(guessing);
-		GuessResult result{};
-
-		for (auto ch : guessing)
-		{
-			if (solution_map[ch - '0'] == true)
-			{
-				result.strikes++;
-			}
-			else
-			{
-				result.balls++;
-			}
-		}
-		if (result.strikes == 3)
-		{
-			result.solved = true;
-		}
-		return result;
-	}
+	GuessResult guess(string guessing);
+	GuessResult getResult(std::string& guessing);
 private:
-	void verifyArgument(std::string& guessing)
-	{
-		if (3 != guessing.size())
-		{
-			throw length_error("must be 3 letters");
-		}
-		for (auto ch : guessing)
-		{
-			if (isNumber(ch)) continue;
-			throw invalid_argument("must be number");
-		}
-		if (hasSameNumber(guessing))
-		{
-			throw invalid_argument("must not have same number");
-		}
-	}
+	void verifyArgument(std::string& guessing);
 
-	bool hasSameNumber(std::string& guessing)
-	{
-		return (guessing[0] == guessing[1]) ||
-			(guessing[1] == guessing[2]) ||
-			(guessing[2] == guessing[0]);
-	}
+	bool hasSameNumber(std::string& guessing);
 
-	bool isNumber(char ch)
-	{
-		return '0' <= ch && '9' >= ch;
-	}
-	string solution;
+	bool isNumber(char ch);
 	bool solution_map[10]{ false };
 };
 
-Baseball::Baseball(string _solution) : solution(_solution)
+Baseball::Baseball(string _solution)
+{
+	buildSolutionMap(_solution);
+}
+
+void Baseball::buildSolutionMap(string& solution)
 {
 	for (auto ch : solution)
 	{
@@ -79,4 +41,60 @@ Baseball::Baseball(string _solution) : solution(_solution)
 
 Baseball::~Baseball()
 {
+}
+
+GuessResult Baseball::guess(string guessing)
+{
+	verifyArgument(guessing);
+	return getResult(guessing);
+}
+
+GuessResult Baseball::getResult(std::string& guessing)
+{
+	GuessResult result{};
+	for (auto ch : guessing)
+	{
+		if (solution_map[ch - '0'] == true)
+		{
+			result.strikes++;
+		}
+		else
+		{
+			result.balls++;
+		}
+	}
+	if (result.strikes == 3)
+	{
+		result.solved = true;
+	}
+	return result;
+}
+
+inline void Baseball::verifyArgument(std::string& guessing)
+{
+	if (3 != guessing.size())
+	{
+		throw length_error("must be 3 letters");
+	}
+	for (auto ch : guessing)
+	{
+		if (isNumber(ch)) continue;
+		throw invalid_argument("must be number");
+	}
+	if (hasSameNumber(guessing))
+	{
+		throw invalid_argument("must not have same number");
+	}
+}
+
+inline bool Baseball::hasSameNumber(std::string& guessing)
+{
+	return (guessing[0] == guessing[1]) ||
+		(guessing[1] == guessing[2]) ||
+		(guessing[2] == guessing[0]);
+}
+
+inline bool Baseball::isNumber(char ch)
+{
+	return '0' <= ch && '9' >= ch;
 }
